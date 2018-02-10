b7cd0cf3f0cff2effe5771cc4788fd184adc48f6
itproto <itprotoltd@gmail.com>
2018-02-10 00:17:54 +0000

    Spike forproxy middleware. Plugged firebase etc

---
+++ src/containers/App/firebase-config.ts
@@ -0,0 +1,9 @@
+const config = {
+  apiKey: "AIzaSyBAjzbk46zfYcQqzj_RRc0due79bkhoURs",
+  authDomain: "reddit-clone-b6557.firebaseapp.com",
+  databaseURL: "https://reddit-clone-b6557.firebaseio.com",
+  projectId: "reddit-clone-b6557",
+  storageBucket: "reddit-clone-b6557.appspot.com",
+  messagingSenderId: "1086898679546"
+};
+export default config;
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,12 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import App from "./App";
 import "./index.css";
-import registerServiceWorker from "./registerServiceWorker";
+import registerServiceWorker from "./utils/registerServiceWorker";
 
-ReactDOM.render(<App />, document.getElementById("root") as HTMLElement);
+import { browserHistory } from "react-router";
+import Routes from "./routes";
+
+ReactDOM.render(<Routes history={browserHistory} />, document.getElementById(
+  "root"
+) as HTMLElement);
 registerServiceWorker();
