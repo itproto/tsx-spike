4c4724d7bac25a6904c53358fd11b9c877991e6a
itproto <itprotoltd@gmail.com>
2018-02-11 14:33:55 +0000

    Refactored and linted middleware files

--- config/middleware/serviceFacade.js
+++ config/middleware/serviceFacade.js
@@ -2,57 +2,21 @@ const methods = {
   POST: "POST",
   GET: "GET"
 };
-const path = require("path");
-const fs = require("fs");
-const shortid = require("shortid");
-
-const { promisify } = require("util");
-
+const { cuid } = require("./utils");
+const { readMockFile, writeMockFile } = require("./mock");
 const { renderAdmin } = require("./admin");
 const { proxyRoute } = require("./proxy");
 
-const readFileAsync = promisify(fs.readFile);
-const writeFileAsync = promisify(fs.writeFile);
-
-const uid = () => shortid.generate();
-
-const mapToArray = map => Array.from(map.entries());
-const arrayToMap = arr => new Map(arr);
 const savedUrls = new Map();
 
 const createRoute = (route, err, json) => {
   return {
-    id: uid(),
+    id: cuid(),
     route,
     err,
     lastUpdated: +new Date(),
     json
   };
-};
-
-const getMockPath = route =>
-  path.join(__dirname, "resp", route.replace(/\//g, "$") + ".json");
-
-const readMockFile = async route => {
-  try {
-    const filePath = getMockPath(route);
-    const text = await readFileAsync(filePath);
-    return JSON.parse(text);
-  } catch (err) {
-    if (err.code === "ENOENT") {
-      return undefined;
-    }
-    throw err;
-  }
-};
-const writeMockFile = async (route, json) => {
-  try {
-    const filePath = getMockPath(route);
-    const content = JSON.stringify(json, undefined, "\t");
-    await writeFileAsync(filePath, content);
-  } catch (err) {
-    throw err;
-  }
 };
 
 const renderCreate = (req, res, route) => {
@@ -64,19 +28,18 @@ const renderCreate = (req, res, route) => {
 
 module.exports = function createServiceFacadeMiddleware(apiUrl) {
   const re = new RegExp(`/${apiUrl}/(.+)`);
-  return async (req, res, next) => {
+  return function(req, res, next) {
     const match = req.url.replace(/\/$/, "").match(re);
     if (!match) {
       next();
       return;
     }
-    const { method, body } = req;
+    const { method } = req;
     const route = match[1];
 
     if (route.match("admin")) {
       return renderAdmin(req, res);
     }
-    const savePath = path.join(__dirname, `${route}.json`);
     switch (method) {
       case methods.GET:
         proxyRoute(req, res, route, async (err, json) => {
