addfc53fc549ebbb485969b6b6faf7d4d4bfed9e
itproto <itprotoltd@gmail.com>
2018-02-11 01:22:02 +0000

    Send mock file for missing routes

--- config/middleware/admin.js
+++ config/middleware/admin.js
@@ -30,32 +30,7 @@ let items = [
     }
   }
 ];
-const { streamToString, parseQS, getIn } = require("./utils");
-
-const updateItems = (items, formUpdate) => {
-  //parse form {'uid1_proxy': 'on', 'uid2_mock': 'on'} => {uid1: {proxy: true}, ...}
-  //parses only checkboxes
-  const flags = Object.keys(formUpdate).reduce((res, key) => {
-    const matches = key.match(/^(.+)_(\w+)$/);
-    if (!matches) {
-      return res;
-    }
-    const [, id, metaProp] = matches;
-    res[id] = res[id] || [];
-    res[id][metaProp] = true;
-    return res;
-  }, {});
-
-  return items.map(item => {
-    return {
-      ...item,
-      meta: {
-        proxy: getIn(item.id, "proxy")(flags),
-        mock: getIn(item.id, "mock")(flags)
-      }
-    };
-  });
-};
+const { streamToString, parseQS, updateItems } = require("./utils");
 
 const renderAdminGet = (req, res, items) => {
   const $res = $tmplPage($routesForm(items));
