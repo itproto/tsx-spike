2a3aaecb8224d8b2867b7c22daf59300479a95d0
itproto <itprotoltd@gmail.com>
2018-02-11 01:22:02 +0000

    Send mock file for missing routes

--- config/middleware/serviceFacade.js
+++ config/middleware/serviceFacade.js
@@ -8,42 +8,48 @@ const shortid = require("shortid");
 
 const { promisify } = require("util");
 
-const request = require("request");
 const { renderAdmin } = require("./admin");
+const { proxyRoute } = require("./proxy");
 
 const readFileAsync = promisify(fs.readFile);
 
-const { streamToString } = require("./utils");
-
 const uid = () => shortid.generate();
-
-function _proxy(req, res, route, cb) {
-  const newUrl = "https://dog.ceo/api/" + route;
-  let r = request(newUrl);
-  if (req.method === methods.POST) {
-    r = request.post({ uri: newUrl, json: req.body });
-  }
-
-  req
-    .pipe(r)
-    .on("response", (response, body) => {
-      if (response.statusCode !== 200) {
-        cb(response.statusCode, undefined);
-        return;
-      }
-      streamToString(response, (err, json) => cb(undefined, JSON.parse(json)));
-    })
-    .on("error", err => cb(err, undefined))
-    .pipe(res);
-}
 
 const mapToArray = map => Array.from(map.entries());
 const arrayToMap = arr => new Map(arr);
 const savedUrls = new Map();
+
+const createRoute = (route, err, json) => {
+  return {
+    id: uid(),
+    route,
+    err,
+    lastUpdated: +new Date(),
+    json
+  };
+};
+
+const readMockFile = async route => {
+  try {
+    const filePath = path.join(
+      __dirname,
+      "resp",
+      route.replace("/", "$") + ".json"
+    );
+    const text = await readFileAsync(filePath);
+    return JSON.parse(text);
+  } catch (err) {
+    if (err.code === "ENOENT") {
+      return ["No Mock"];
+    }
+    throw err;
+  }
+};
+
 module.exports = function createServiceFacadeMiddleware(apiUrl) {
-  const re = new RegExp(`/${apiUrl}/(.*)`);
+  const re = new RegExp(`/${apiUrl}/(.+)`);
   return async (req, res, next) => {
-    const match = req.url.match(re);
+    const match = req.url.replace(/\/$/, "").match(re);
     if (!match) {
       next();
       return;
@@ -57,27 +63,16 @@ module.exports = function createServiceFacadeMiddleware(apiUrl) {
     const savePath = path.join(__dirname, `${route}.json`);
     switch (method) {
       case methods.GET:
-        _proxy(req, res, route, (err, json) => {
-          savedUrls.set(route, {
-            id: uid,
-            json,
-            err,
-            lastUpdated: +new Date()
-          });
+        proxyRoute(req, res, route, async (err, json) => {
+          savedUrls.set(route, createRoute(route));
+          if (err) {
+            const mjson = await readMockFile(route);
+            return res.json(mjson);
+          }
 
           console.info(JSON.stringify(mapToArray(savedUrls)));
         });
         return;
-        try {
-          const text = await readFileAsync(savePath);
-          res.json(JSON.parse(text));
-        } catch (err) {
-          if (err.code === "ENOENT") {
-            res.json([]);
-            return;
-          }
-          throw err;
-        }
         break;
       case methods.POST:
         res.json({ error: "NOT_IML" });
--- config/middleware/utils.js
+++ config/middleware/utils.js
@@ -26,4 +26,29 @@ const parseQS = qs => {
 const getIn = (...p) => o =>
   p.reduce((xs, x) => (xs && xs[x] ? xs[x] : undefined), o);
 
-module.exports = { streamToString, parseQS, getIn };
+const updateItems = (items, formUpdate) => {
+  //parse form {'uid1_proxy': 'on', 'uid2_mock': 'on'} => {uid1: {proxy: true}, ...}
+  //parses only checkboxes
+  const flags = Object.keys(formUpdate).reduce((res, key) => {
+    const matches = key.match(/^(.+)_(\w+)$/);
+    if (!matches) {
+      return res;
+    }
+    const [, id, metaProp] = matches;
+    res[id] = res[id] || [];
+    res[id][metaProp] = true;
+    return res;
+  }, {});
+
+  return items.map(item => {
+    return {
+      ...item,
+      meta: {
+        proxy: getIn(item.id, "proxy")(flags),
+        mock: getIn(item.id, "mock")(flags)
+      }
+    };
+  });
+};
+
+module.exports = { streamToString, parseQS, getIn, updateItems };
