0e9742ebb073faeef48250e0363d8cebc6d18d45
itproto <itprotoltd@gmail.com>
2018-02-15 21:37:31 +0000

    Implemented reddit clone with voting

--- src/containers/App/index.tsx
+++ src/containers/App/index.tsx
@@ -3,13 +3,48 @@ import "./App.css";
 
 import * as firebase from "firebase";
 import config from "./firebase-config";
+import { ReactElement } from "react";
 
 class App extends React.Component<any, any> {
-  componentDidMount() {
+  state = { posts: [], loading: false };
+  constructor(props: any) {
+    super(props);
     firebase.initializeApp(config);
   }
+
+  get postsRef() {
+    return firebase.database().ref("posts");
+  }
+
+  get fdb() {
+    return firebase.database();
+  }
+
+  componentWillMount() {
+    this.postsRef.on("value", (snapshot: any) => {
+      console.log(snapshot.val());
+
+      this.setState({
+        posts: snapshot.val(),
+        loading: false
+      });
+    });
+  }
+
   public render() {
-    return <div className="App">Hello World</div>;
+    const { children } = this.props;
+    const { posts, loading } = this.state;
+    return (
+      <div className="App">
+        {children &&
+          React.cloneElement(children as ReactElement<any>, {
+            posts,
+            loading,
+            postsRef: this.postsRef,
+            fdb: this.fdb
+          })}
+      </div>
+    );
   }
 }
 
