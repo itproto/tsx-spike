44fb54ea9f6edbad6ae604c53958a5185cf46130
itproto <itprotoltd@gmail.com>
2018-02-15 21:42:03 +0000

    Cleanup

--- src/containers/App/index.tsx
+++ src/containers/App/index.tsx
@@ -1,13 +1,7 @@
 import * as React from "react";
 import "./App.css";
 
-import * as firebase from "firebase";
-import config from "./firebase-config";
-
 class App extends React.Component<any, any> {
-  componentDidMount() {
-    firebase.initializeApp(config);
-  }
   public render() {
     return <div className="App">Hello World</div>;
   }
